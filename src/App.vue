799c14d798e3ab12345bcfaa6059e708e6c273f3
mvpme82 <mvpme82@163.com>
2022-02-20 12:45:33 +0800

    ccc test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <div>mvpme82</div>
+  <MyNewComp />
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import MyNewComp from "./components/MyNewComp.vue";
 
 export default {
   name: "App",
-  components: { HelloWorld },
+  components: { HelloWorld, MyNewComp },
 };
 </script>
 
